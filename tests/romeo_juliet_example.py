9a2f037ead80e62a8fa4b90de251ff6307b56d10
b3ck3tt <roman.neruda@gmail.com>
2020-04-04 17:28:16 +0200

    Update romeo_juliet_example.py

--- tests/romeo_juliet_example.py
+++ tests/romeo_juliet_example.py
@@ -3,9 +3,15 @@ import networkx as nx
 
 from romeo_juliet_graph_gen import RomeoAndJuliet
 
+# as multigraph - print
 rj = RomeoAndJuliet()
 rj.printMulti()
 
+# as flattenned graph - get and print
 H = rj.asOneGraph()
 dot_H = nx.nx_pydot.to_pydot(H)
 print(dot_H)
+
+# as a dictionary of simple graphs indexed by layer codes
+MyGraphs=rj.asDictOfGraphs()
+print(MyGraphs)
