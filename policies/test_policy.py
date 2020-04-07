74dface7e6dd12fa69f774fd1e43840988023d5f
Petra Vidnerova <petra.vidnerova@gmail.com>
2020-04-08 01:39:38 +0200

    base model moved

--- policies/test_policy.py
+++ policies/test_policy.py
@@ -7,11 +7,19 @@ class Policy():
         # ?? do changes in place or shall I make a copy of a graph?
         # to do go through history
 
+        quarantine = {
+            layer: 0.1 for layer in self.graph.layer_names
+        }
+        quarantine["F"] = 100
+
+        # overkill,  budou se brat jen ty, co se presli do Id dnes
         detected_states = [
             idx for idx, x in enumerate(states) if x == "I_d"
         ]
+        return
+
         for node in detected_states:
-            self.graph.isolate_node(node)
+            self.graph.modify_layers_for_node(node, quarantine)
 
         to_change = {"graph": self.graph}
         return to_change
