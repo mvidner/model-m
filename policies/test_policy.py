64397a9df875699ea049030d8e10f0c33f547cab
Petra Vidnerova <petra.vidnerova@gmail.com>
2020-04-09 17:28:06 +0000

    fixed for seirsplus graph

--- policies/test_policy.py
+++ policies/test_policy.py
@@ -1,25 +1,38 @@
 from run_experiment import matrix  # TODO what to DO?
-
+from graph_gen import GraphGenerator
 
 def simple_policy(graph, states, history):
     # ?? do changes in place or shall I make a copy of a graph?
     # to do go through history
     print("Hello world! This is the policy function speaking.")
 
-    quarantine = {
-        layer: 0 for layer in graph.layer_names
-    }
-    #    quarantine["F"] = 100
+    if isinstance(graph, GraphGenerator):
+        quarantine = {
+            layer: 0 for layer in graph.layer_names
+        }
+        #    quarantine["F"] = 100
 
-    # overkill,  budou se brat jen ty, co se presli do Id dnes
-    nodes = list(graph.G.nodes)
-    detected_states = [
-        nodes[idx] for idx, x in enumerate(states) if x == "I_d"
-    ]
+        # overkill,  budou se brat jen ty, co se presli do Id dnes
+        nodes = list(graph.G.nodes)
+        detected_nodes = [
+            nodes[idx] for idx, x in enumerate(states) if x == "I_d"
+        ]
 
-    for node in detected_states:
-        graph.modify_layers_for_node(node, quarantine)
-    A = matrix(graph)
+        for node in detected_nodes:
+            graph.modify_layers_for_node(node, quarantine)
+        A = matrix(graph)
+    else:
+        # just for testing seirsplus example graph 
+        nodes = list(graph.nodes)
+        detected_nodes = [
+            nodes[idx] for idx, x in enumerate(states) if x == "I_d"
+        ]
+        for node in detected_nodes:
+            for e in graph.edges([node], data=True):
+                graph.edges[e[:2]]['weight'] = 0.0
+        A = graph     
 
+        
+        
     to_change = {"graph": A}
     return to_change
