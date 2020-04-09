602d94c81d6010c2a6af4156071ee97bf8c49f8d
Petra Vidnerova <petra.vidnerova@gmail.com>
2020-04-09 14:07:30 +0200

    simple plot

--- utils/plot_utils.py
+++ utils/plot_utils.py
@@ -1,19 +1,16 @@
 # vzkaz pro Gabinu: smaz tu a prepis tu cokoli budes chtit
 
 import pandas as pd
+import matplotlib.pyplot as plt
+
 
 def plot_history(filename):
 
     history = pd.read_csv(filename)
-    print(history)
-    
-    # counts = [model.state_counts[s].asarray()
-    #           for s in ("I_n", "I_a", "I_s", "I_d", "E")]
-    y = np.sum(counts, axis=0)
-    x = model.tseries.asarray()
-    plt.plot(x, y)
-    test_id = "_" + test_id if test_id else ""
-    plt.savefig(f"num_of_ill{test_id}.png")
+    history["all_infectious"] = history[[
+        "I_n", "I_a", "I_s", "I_d", "E"]].sum(axis=1)
+    history.plot(y="all_infectious")
+    plt.show()
 
 
 if __name__ == "__main__":
