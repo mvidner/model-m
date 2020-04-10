a4f9b5af024c50ab6ff9c9db6268f90be3d92fbe
Petra Vidnerova <petra.vidnerova@gmail.com>
2020-04-10 10:26:34 +0200

    space for playing with plots

--- tests/run.sh
+++ tests/run.sh
@@ -1,4 +1,9 @@
-ID="test"
+ID="no_policy"
 for I in `seq  0 9`; do
-    python run_experiment.py -r example.ini  ${ID}_$I > ${ID}_$I.log &
+    python run_experiment.py -r romeo_and_juliet.ini ${ID}_$I > ${ID}_$I.log 
+done
+
+ID="quarantine"
+for I in `seq  0 9`; do
+    python run_experiment.py -r romeo_and_julietQ.ini ${ID}_$I > ${ID}_$I.log 
 done
