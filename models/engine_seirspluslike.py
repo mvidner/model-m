computation of P_infection fixed

models/engine_seirspluslike.py
@@ -6,7 +6,7 @@ import networkx as nx
 import time
 
 from history_utils import TimeSeries, TransitionHistory
-from sparse_utils import prop_of_column
+from sparse_utils import prop_of_row
 from engine import BaseEngine
 
 
@@ -30,6 +30,9 @@ class SeirsPlusLikeEngine(BaseEngine):
             else:
                 setattr(self, param_name,
                         np.full(fill_value=param, shape=(self.num_nodes, 1)))
+            #print(param_name, getattr(self, param_name))
+#        exit()
+
 
     def setup_series_and_time_keeping(self):
 
@@ -159,18 +162,38 @@ class SeirsPlusLikeEngine(BaseEngine):
             raise TypeException(
                 "num_contacts(state) accepts str or list of strings")
 
-    def prob_of_no_contact(self, states):
-        assert type(states) == list
-        state_flags = self.memberships[states, :, :].reshape(
-            len(states), self.num_nodes).sum(axis=0)
-        print("state flags", state_flags.shape)
+    def prob_of_no_contact(self, source_states, dest_states, beta):
+        #        print(states)
+        #for i in states:
+        #    print(self.current_state_count(i))
+        assert type(dest_states) == list and type(source_states) == list 
+        dest_flags = self.memberships[dest_states, :, :].reshape(
+            len(dest_states), self.num_nodes).sum(axis=0)
+        source_flags = self.memberships[source_states, :, :].reshape(
+            len(source_states), self.num_nodes).sum(axis=0)
+        #        print("state flags", state_flags.shape)
+        #        print(self.node_ids[state_flags == 1])
 
-        vysek = self.A[state_flags == 1, :]
-        #        print(vysek.shape)
+        vysek = self.A[source_flags == 1, :][:, dest_flags == 1] 
         vysek.eliminate_zeros()
-        not_prob_contact = scipy.sparse.csr_matrix(vysek.shape)
-        not_prob_contact.data = 1.0 - vysek.data
-        return 1 - prop_of_column(not_prob_contact).reshape(-1, 1)
+        #        print(vysek.shape)
+        if vysek.shape[0] == 0: 
+            return np.zeros((self.num_nodes, 1))
+        not_prob_contact = scipy.sparse.csr_matrix(vysek)
+        assert np.all(not_prob_contact.data >= 0) and np.all(not_prob_contact.data <= 1)
+        #        print(not_prob_contact)
+        beta = np.tile(beta[dest_flags==1].ravel(), (not_prob_contact.shape[0], 1))
+        #print(not_prob_contact.shape, beta.shape)
+        not_prob_contact = scipy.sparse.csr_matrix(not_prob_contact.multiply(beta))
+        not_prob_contact.data = 1.0 - not_prob_contact.data
+        # print("**** == 1", np.all(not_prob_contact.data == 1))
+        # print("**** prop columns", prop_of_column(not_prob_contact), np.all(prop_of_column(not_prob_contact)==1))
+        # print("*** contact ",  1 - prop_of_column(not_prob_contact))
+        result = np.zeros(self.num_nodes)
+        #print((1 - prop_of_row(not_prob_contact)).shape, result.shape, 
+        #      result[source_flags == 1].shape)
+        result[source_flags == 1] = 1 - prop_of_row(not_prob_contact)
+        return result.reshape(self.num_nodes, 1) 
 
         # not_prob_contact = prob_contact
         # not_prob_contact.data = 1.0 - not_prob_contact.data

models/engine_sequential.py
@@ -34,13 +34,18 @@ class SequentialEngine(SeirsPlusLikeEngine):
 
         plist = self.calc_propensities()
         propensities = np.column_stack(plist)
+
+        assert np.all(propensities >= 0) and np.all(propensities <= 1), \
+            f">=0 & <= 0 failed for {propensities[propensities >= 0]} a \
+            {propensities[propensities<=1]} " 
         # check
         # print(propensities.shape)
         # print(self.memberships.shape)
         # print("node 0", self.memberships[:, 0].flatten())
         # print(propensities[0])
-        # print(propensities.sum(axis=1))
-        assert np.all(propensities.sum(axis=1) == 1)
+        # print(propensities.sum(axis=1q))
+        
+        assert np.allclose(propensities.sum(axis=1), 1.0)
 
         # add column with pst P[X->X]
         # what is the fastest way to add a column?
@@ -111,6 +116,8 @@ class SequentialEngine(SeirsPlusLikeEngine):
         self.print(True)
 
         for self.t in range(1, T+1):
+            if __debug__:
+                print(flush=True)
             #            print(f"day {self.t}")
 
             print(self.t)
@@ -143,6 +150,15 @@ class SequentialEngine(SeirsPlusLikeEngine):
                         ])
             if not numI > 0:
                 break
+
+        if self.t < T:
+            for t in range(self.t+1, T+1):
+                if (t >= len(self.state_counts[0])):
+                    self.increase_data_series_length()
+                for state in self.states:
+                    self.state_counts[state][t] = self.state_counts[state][t-1]
+        self.t = T
+
 
         self.print(verbose)
         self.finalize_data_series()

models/extended_network_model.py
@@ -41,23 +41,6 @@ state_codes = {
 }
 
 
-# constants - have to be consisten with transtion list
-S_to_S_s = 0
-S_to_E = 1
-S_s_to_S = 2
-S_s_to_E = 3
-E_to_I_n = 4
-E_to_I_a = 5
-I_n_to_R_u = 6
-I_a_to_I_s = 7
-I_s_to_R_u = 8
-I_s_to_D_u = 9
-I_s_to_I_d = 10
-I_d_to_R_d = 11
-I_d_to_D_d = 12
-I_a_to_I_d = 13
-E_to_I_d = 14
-
 
 # MODEL DEFINITION
 
@@ -123,7 +106,12 @@ model_definition = {
 
         (STATES.I_d, STATES.R_d),
         (STATES.I_d, STATES.D_d),
-        (STATES.I_d, STATES.I_d)
+        (STATES.I_d, STATES.I_d),
+
+        (STATES.R_d, STATES.R_d),
+        (STATES.R_u, STATES.R_u),
+        (STATES.D_d, STATES.D_d),
+        (STATES.D_u, STATES.D_u)
     ],
 
     "final_states": [
@@ -215,12 +203,29 @@ def calc_propensities(model, use_dict=True):
         # ) / model.current_N()
         # + (1 - model.p)
 
-    P_infection = (
-        model.beta *
-        model.prob_of_no_contact(
-            [STATES.I_n, STATES.I_a, STATES.I_s])
-        + model.beta_D * model.prob_of_no_contact([STATES.I_d])
+   
+    P1 = model.prob_of_no_contact(
+        [STATES.S_s, STATES.S],
+        [STATES.I_n, STATES.I_a, STATES.I_s],
+        model.beta
     )
+
+    #    P2 = model.prob_of_no_contact([STATES.I_d], model.beta_D)
+    assert(np.all(model.beta_D == 0))
+    
+
+    #print("-->", P1.shape, np.any(P1.flatten() > 0), np.all(P1.flatten() <= 1))
+    #print(P2.flatten())
+    #assert np.all(P2.flatten() == 0)
+
+    #    P_infection = model.prob_of_no_contact(
+    #        ([STATES.I_n, STATES.I_a, STATES.I_s], [STATES.I_d])
+    #        (model.beta, model.beta_D)
+    #    )
+    #    print("-->", P_infection.shape, np.any(P_infection.flatten() > 0), np.all(P_infection.flatten() <= 1)) 
+    
+    P_infection = P1
+
     #    print(P_infection.shape)
 
     not_P_infection = 1 - P_infection
@@ -234,7 +239,7 @@ def calc_propensities(model, use_dict=True):
     # print((model.memberships[STATES.S_s] * not_P_infection).shape)
     # exit()
 
-    print(model.memberships[:, 0])
+    #    print(model.memberships[:, 0])
     # state S_s
     propensity_S_s_to_E = model.memberships[STATES.S_s] * P_infection
     propensity_S_s_to_S = (
@@ -254,12 +259,13 @@ def calc_propensities(model, use_dict=True):
         model.memberships[STATES.S] * not_P_infection * model.false_symptoms_rate)
     propensity_S_to_S = model.memberships[STATES.S] * \
         (1.0 - (propensity_S_to_E + propensity_S_to_S_s))
-    print("S->E", propensity_S_to_E[0])
-    print("S->Ss", propensity_S_to_S_s[0])
-    print("E+Ss", (propensity_S_to_E + propensity_S_to_S_s)[0])
-    print("E+Ss", (1.0 - (propensity_S_to_E + propensity_S_to_S_s))[0])
-    print(model.memberships[STATES.S][0])
-    print("S->S", propensity_S_to_S[0])
+
+    #    print("S->E", propensity_S_to_E[0])
+    #    print("S->Ss", propensity_S_to_S_s[0])
+    #    print("E+Ss", (propensity_S_to_E + propensity_S_to_S_s)[0])
+    #    print("E+Ss", (1.0 - (propensity_S_to_E + propensity_S_to_S_s))[0])
+    #    print(model.memberships[STATES.S][0])
+    #    print("S->S", propensity_S_to_S[0])
 
     # state E
     propensity_E_to_I_d = (model.memberships[STATES.E] *
@@ -304,6 +310,14 @@ def calc_propensities(model, use_dict=True):
     propensity_I_d_to_I_d = model.memberships[STATES.I_d] * (
         1.0 - propensity_I_d_to_R_d - propensity_I_d_to_D_d)
 
+    # state R_d, R_u, D_d, D_u        
+    propensity_R_d_to_R_d = model.memberships[STATES.R_d]
+    propensity_R_u_to_R_u = model.memberships[STATES.R_u]
+    propensity_D_d_to_D_d = model.memberships[STATES.D_d]
+    propensity_D_u_to_D_u = model.memberships[STATES.D_u]
+
+    
+
     return [
         propensity_S_s_to_E,
         propensity_S_s_to_S,
@@ -333,7 +347,11 @@ def calc_propensities(model, use_dict=True):
 
         propensity_I_d_to_R_d,
         propensity_I_d_to_D_d,
-        propensity_I_d_to_I_d
+        propensity_I_d_to_I_d,
+        propensity_R_d_to_R_d,
+        propensity_R_u_to_R_u,
+        propensity_D_d_to_D_d,
+        propensity_D_u_to_D_u
     ]
 
 

utils/sparse_utils.py
@@ -6,6 +6,7 @@ def multiply_row(A, row_idx, alpha, trunc=False):
     '''
     multiply values in row_idx in place
     '''
+
     idx_start_row = A.indptr[row_idx]
     idx_end_row = A.indptr[row_idx + 1]
 
@@ -27,14 +28,34 @@ def multiply_col(A, col_idx, alpha, trunc=False):
         A.data[col_indices] = np.clip(A.data[col_indices], 0.0, 1.0)
 
 
+def prop_of_row(A):
+
+    result = np.ones(A.shape[0])
+
+    i = 0 
+    n = len(A.indptr)
+    while i < n-1: 
+        s, e = A.indptr[i], A.indptr[i+1]
+        result[i] = np.prod(A.data[s:e])
+        i += 1 
+    if A.indptr[i] < len(A.data):
+        s = A.indptr[i] 
+        result[i] = np.prod(A.data[s:])
+    return result 
+
+
 def prop_of_column(A):
 
     result = np.ones(A.shape[1])
     col_indices = A.indices
+    #    print("columns", np.unique(col_indices))
+
+    # print(".... prop_of_column fce ", A.indices, np.unique(col_indices), A.data)
 
     for col_idx in np.unique(col_indices):
         current_indices = A.indices == col_idx
-        result[col_idx] = np.prod(1 - A.data[current_indices])
+        #        print(" .... ", A.data[current_indices], np.prod(1 - A.data[current_indices]))
+        result[col_idx] = np.prod(A.data[current_indices])
     return result
 
 