0e717a44d7c518d297216e0e78d3449c3aed7cc3
Gabriela Suchoparova <gabi.suchoparova@gmail.com>
2020-04-25 16:37:49 +0200

    Basic iface for hyperparam search, also moved config loading from tests to models

--- tests/run_experiment.py
+++ tests/run_experiment.py
@@ -2,84 +2,15 @@ import timeit
 import time
 import click
 import random
-import networkx as nx
 import matplotlib.pyplot as plt
 import numpy as np
-import pandas as pd
-from scipy.sparse import csr_matrix
 
 from config_utils import ConfigFile
-from sparse_utils import multiply_zeros_as_ones
-from graph_gen import GraphGenerator, CSVGraphGenerator, RandomSingleGraphGenerator, RandomGraphGenerator
-from csv_graph import CSVGraph
-from light_graph import LightGraph
+from graph_gen import GraphGenerator
+from load_model import create_graph, matrix, load_model_from_config
 from policy import bound_policy
 
-# from seirs_extended import ExtendedNetworkModel, custom_exponential_graph
 from model_zoo import model_zoo
-from seirs import custom_exponential_graph
-
-
-verona_available = True
-try:
-    from romeo_juliet_graph_gen import RomeoAndJuliet as Verona
-except ModuleNotFoundError:
-    verona_available = False
-
-
-def magic_formula(graph):
-
-    # rozvrtany ... nutno opravit
-
-    N = graph.number_of_nodes()
-
-    #    print(graph.get_layers_info())
-
-    prob_no_contact = csr_matrix((N, N))  # empty values = 1.0
-
-    for name, prob in graph.get_layers_info().items():
-        a = nx.adj_matrix(graph.get_graph_for_layer(name))
-        if len(a.data) == 0:
-            continue
-        a = a.multiply(prob)  # contact on layer
-        not_a = a  # empty values = 1.0
-        not_a.data = 1.0 - not_a.data
-        prob_no_contact = multiply_zeros_as_ones(prob_no_contact, not_a)
-        del a
-
-    # probability of contact (whatever layer)
-    prob_of_contact = prob_no_contact
-    prob_of_contact.data = 1.0 - prob_no_contact.data
-    return prob_of_contact
-
-
-def create_graph(name, nodes="nodes.csv", edges="edges.csv", layers="etypes.csv", num_nodes=None):
-
-    if name == "romeo_and_juliet":
-        if not verona_available:
-            raise NotImplementedError(
-                "Verona not available. Contact Roman Neruda for source files.")
-        else:
-            return Verona(random_seed=7)
-
-    if name == "csv":
-        return CSVGraphGenerator(path_to_nodes=nodes, path_to_edges=edges, path_to_layers=layers)
-
-    if name == "csv_petra":
-        return CSVGraph(path_to_nodes=nodes, path_to_edges=edges, path_to_layers=layers)
-
-    if name == "csv_light":
-        return LightGraph(path_to_nodes=nodes, path_to_edges=edges, path_to_layers=layers)
-
-    if name == "seirsplus_example":
-        base_graph = nx.barabasi_albert_graph(n=num_nodes, m=9, seed=7)
-        np.random.seed(42)
-        return custom_exponential_graph(base_graph, scale=100)
-
-    if name == "random":
-        return RandomGraphGenerator()
-
-    raise ValueError(f"Graph {name} not available.")
 
 
 def tell_the_story(history, graph):
@@ -128,99 +59,19 @@ def tell_the_story(history, graph):
     return "\n".join(story)
 
 
-def matrix(graph, cf):
-
-    if cf:
-        scenario = cf.section_as_dict("SCENARIO")
-    else:
-        scenario = False 
-
-    if isinstance(graph, CSVGraph):
-        if scenario:
-            raise NotImplementedError(
-                "CSVGraph does not support closing layers yet.")
-        return graph.G
-
-    if isinstance(graph, LightGraph):
-        if scenario:
-            raise NotImplementedError(
-                "LightGraph does not support closing layers yet.")
-        return graph.A
-
-    if isinstance(graph, RandomSingleGraphGenerator):
-        if scenario:
-            raise NotImplementedError(
-                "RandomGraphGenerator does not support closing layers.")
-        return graph.G
-
-    if isinstance(graph, GraphGenerator):
-        if scenario:
-            list_of_closed_layers = scenario["closed"]
-            graph.close_layers(list_of_closed_layers)
-        return magic_formula(
-            graph
-        )
-    else:
-        return None
-
-
 def demo(filename, test_id=None, model_random_seed=42, use_policy=None, print_interval=1):
 
     cf = ConfigFile()
     cf.load(filename)
 
-    num_nodes = cf.section_as_dict("TASK").get("num_nodes", None)
-    model_params = cf.section_as_dict("MODEL")
+    model, run_params = load_model_from_config(cf, model_random_seed=model_random_seed, use_policy=use_policy)
 
-    graph_name = cf.section_as_dict("GRAPH")["name"]
-    nodes = cf.section_as_dict("GRAPH").get("nodes", "nodes.csv")
-    edges = cf.section_as_dict("GRAPH").get("edges", "edges.csv")
-    layers = cf.section_as_dict("GRAPH").get("layers", "etypes.csv")
-
-    start = time.time()
-    graph = create_graph(graph_name, nodes=nodes, edges=edges,
-                         layers=layers, num_nodes=num_nodes)
-
-    A = matrix(graph, cf)
-    end = time.time()
-    print("Graph loading: ", end-start, "seconds")
-
-    #    print(graph)
-
-    class_name = cf.section_as_dict("TASK").get(
-        "model", "ExtendedNetworkModel")
-    Model = model_zoo[class_name]
-    model = Model(G=graph if A is None else A,
-                  **model_params,
-                  random_seed=model_random_seed)
-
-    if use_policy:  # TODO: cfg versus --policy option
-        policy_cfg = cf.section_as_dict("POLICY")
-        if use_policy not in policy_cfg["name"]:
-            raise ValueError("Unknown policy name.")
-
-        if policy_cfg and "filename" in policy_cfg:
-            policy = getattr(__import__(
-                policy_cfg["filename"]), use_policy)
-            policy = bound_policy(policy, graph)
-            model.set_periodic_update(policy)
-        else:
-            print("Warning: NO POLICY IN CFG")
-            print(policy_cfg)
-
-    print(model.__class__.__name__)
-    print(model)
-
-    ndays = cf.section_as_dict("TASK").get("duration_in_days", 60)
-    print_interval = cf.section_as_dict("TASK").get("print_interval", 1)
-    verbose = cf.section_as_dict("TASK").get("verbose", "Yes") == "Yes"
-
-    model.run(T=ndays, verbose=verbose, print_interval=print_interval)
+    model.run(**run_params)
     print("Avg. number of events per day: ", model.tidx/ndays)
 
     storyfile = cf.section_as_dict("OUTPUT").get("story", None)
     if storyfile:
-        story = tell_the_story(model.history, graph)
+        story = tell_the_story(model.history, model.G)
         with open(storyfile, "w") as f:
             f.write(story)
 
--- utils/hyperparam_utils.py
+++ utils/hyperparam_utils.py
@@ -1,26 +1,65 @@
+import json
+import time
+from functools import partial
+from sklearn.model_selection import ParameterGrid
+
 from joblib import delayed, Parallel
 from typing import Iterable, Dict, Callable
 
+from config_utils import ConfigFile
+from load_model import load_model_from_config
+from model_zoo import model_zoo
+from policy import bound_policy
 
-def run_single_model(model_cls, model, kwargs, ndays, print_interval=10, verbose=False):
+
+def run_single_model(model, ndays, print_interval=10, verbose=False, return_func=None):
     model.run(T=ndays, verbose=verbose, print_interval=print_interval)
 
+    # extract information to return or do some postprocessing
+    if return_func is not None:
+        return return_func(model)
 
-def _hyperparam_search(model_func: Callable,
-                       hyperparam_gen: Iterable[Dict] = None):
-    """
+    return model
 
-    Params:
-        model_func: A function of signature `func(**kwargs)` that creates the model.
-        hyperparam_gen: Generates kwargs - hyperparameters - for model func.
 
-    Returns:
+def run_hyperparam_search(model_config: str, hyperparam_config: str, model_random_seed=42, use_policy=None, **kwargs):
+    cf = ConfigFile()
+    cf.load(model_config)
 
-    """
+    model_func = partial(load_model_from_config, cf, model_random_seed=model_random_seed, use_policy=use_policy)
+    hyperparam_search_func = _init_hyperparam_search(hyperparam_config)
 
-    for hyperparam_dict in hyperparam_gen:
-        yield model_func(hyperparam_dict)
+    return hyperparam_search_func(model_func=model_func, **kwargs)
 
+
+def _init_hyperparam_search(hyperparam_file: str):
+    with open(hyperparam_file, 'r') as json_file:
+        config = json.load(json_file)
+
+    return partial(hyperparam_search_zoo[config["method"]], hyperparam_config=config)
+
+
+def _run_model_with_hyperparams(model_func, hyperparams):
+    model, run_params = model_func(hyperparams=hyperparams)
+    return model.run(**run_params)
+
+
+def perform_gridsearch(model_func, hyperparam_config, n_jobs=1):
+    grid = hyperparam_config["MODEL"]
+    res = Parallel(n_jobs=n_jobs)(
+        delayed(_run_model_with_hyperparams)(model_func, hp)
+        for hp in ParameterGrid(grid)
+    )
+
+    return res
+
+
+
+hyperparam_search_zoo = {
+    'GridSearch': perform_gridsearch
+}
+
+# TODO json with hyperparam list, method specification (search strategy)
 
 # TODO parse configs func
 # TODO create model func - sth that initializes a graph and takes some hyperparams
@@ -38,6 +77,5 @@ graph_name = cf.section_as_dict("GRAPH")["name"]
 """
 
 # TODO also better history grouping for results
-# TODO multiple models - again a generator; init is pairs of config files - orig and list one
-# TODO or one config and if sth is a list, its a generator, else simple model
+# TODO multiple models - again a generator; init is pairs of config files - orig and hyperparam one
 # TODO and different search strategies
