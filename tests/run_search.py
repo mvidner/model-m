77c6a093572a36d4d368e91727e13e741d220b10
PetraVidnerova <petra.vidnerova@gmail.com>
2020-04-27 16:44:47 +0000

    create result file

--- tests/run_search.py
+++ tests/run_search.py
@@ -2,6 +2,7 @@ import datetime
 import os
 import random
 import timeit
+import json
 
 import click
 
@@ -9,6 +10,9 @@ import pandas as pd
 
 from config_utils import ConfigFile
 from hyperparam_utils import run_hyperparam_search
+from functools import partial
+
+from extended_network_model import STATES as s 
 
 
 def save_model_results(model):
@@ -35,6 +39,7 @@ def test(set_random_seed, preload_graph, policy, n_jobs, run_n_times, out_dir, f
 
     if not os.path.exists(out_dir):
         os.mkdir(out_dir)
+
 
     # TODO pro Petru - klidne si to prepis, to je jen pro ukazku, jak ted vypada return_func
     # return_func for every model run
@@ -81,13 +86,30 @@ def test(set_random_seed, preload_graph, policy, n_jobs, run_n_times, out_dir, f
             else:
                 df = model_res.to_df()
                 df.to_csv(f)
+        
                 
-
+    def evaluate_one_result(model_res, hyperparams, model_random_seed, **kwargs):
+        if run_n_times > 1:
+            tseries = [ m.get_state_count(s.I_d).values for m in model_res] 
+            Id_counts = np.mean(tseries)
+        else:
+            Id_counts = model_res.get_state_count(s.I_d).values
+        return hyperparams, Id_counts.max()
+        
+    def return_func(model_res, hyperparams, model_random_seed, **kwargs):
+        save_mean_history(model_res, hyperparams, model_random_seed, **kwargs)
+        return evaluate_one_result(model_res, hyperparams, model_random_seed, **kwargs)
 
     def search_func():
-        return run_hyperparam_search(filename, hyperparam_filename, model_random_seed=random_seed, use_policy=policy,
-                                     n_jobs=n_jobs, return_func=save_mean_history, run_n_times=run_n_times,
+        result_file_name =os.path.join(out_dir, f'result.txt')
+        results = run_hyperparam_search(filename, hyperparam_filename, model_random_seed=random_seed, use_policy=policy,
+                                     n_jobs=n_jobs, return_func=return_func, run_n_times=run_n_times,
                                      preload_graph=preload_graph)
+        with open(result_file_name, "w") as f:
+            for params, value in results:
+                param_str = json.dumps(params)
+                f.write(f"{param_str}, {value}\n")
+
 
     print(timeit.timeit(search_func, number=1))
 
