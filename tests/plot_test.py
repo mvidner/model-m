2f244620f48bf9ee78bf58e125cd604700de77cb
Petra Vidnerova <petra.vidnerova@gmail.com>
2020-05-02 22:53:28 +0200

    conflict fixed

--- tests/plot_test.py
+++ tests/plot_test.py
@@ -1,11 +1,11 @@
 from plot_utils import plot_histories, plot_mutliple_policies, plot_state_histogram, plot_history
 import matplotlib.pyplot as plt
-import sys 
+import sys
 
 
 # import sys
 
-# plot_history(sys.argv[1], savepath=sys.argv[1]+".png")
+# plot_history(sys.argv[1])
 # exit()
 
 
@@ -13,19 +13,17 @@ BASEDIR = "."
 CITY = "town0"
 
 filename = sys.argv[1]
-variants_list = sys.argv[2:] 
+variants_list = sys.argv[2:]
 
 variant_dict = {}
 
 for variant in variants_list:
     variant_dict[variant] = [
         f"{BASEDIR}/history_{variant}_{i}.csv"
-        for i in range(100)
+        for i in range(0, 100)
     ]
 
 
 plt.rcParams["figure.figsize"] = (20, 15)
 plot_mutliple_policies(variant_dict, group_days=2,
                        group_func="max", save_path=filename)
-
-
