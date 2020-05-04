c1ce69a9643e1f8bbf8065b593c0f7f49ef0d45a
b3ck3tt <roman.neruda@gmail.com>
2020-05-04 22:06:11 +0200

    NewGraph() basic funtionality

--- tests/new_graph_example.py
+++ tests/new_graph_example.py
@@ -1,11 +1,13 @@
 from graphviz import Graph
 import networkx as nx
 
-from graph_gen_new import NewGraphGenerator
+from graph_new import NewGraph
 
 prefix = '../data/newtown/'
-gg = NewGraphGenerator()
+gg = NewGraph()
 gg.read_csv(prefix)
 print(nx.info(gg.G))
 
+e = gg.get_edges_for_node(5)
 
+print(e)
