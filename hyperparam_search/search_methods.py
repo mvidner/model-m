4f227e4011298c040702ddb536ee862f6393ef86
Gabriela Suchoparova <gabi.suchoparova@gmail.com>
2020-05-07 23:01:38 +0200

    CMA-ES implementation demo

--- hyperparam_search/search_methods.py
+++ hyperparam_search/search_methods.py
@@ -1,3 +1,5 @@
+import cma
+import numpy as np
 from functools import partial
 from multiprocessing import Pool
 
@@ -23,6 +25,30 @@ def perform_gridsearch(model_func, hyperparam_config, n_jobs=1):
     return res
 
 
+def cma_es(model_func, hyperparam_config, mean_of_runs=False, return_only_best=True, **kwargs):
+    initial_vals = [v for v in hyperparam_config["MODEL"].values()]
+    sigma = hyperparam_config["SIGMA"]
+    cma_kwargs = hyperparam_config["CMA"]
+
+    def keys_with_evolved_vals(evolved_vals):
+        return {k: v for k, v in zip(hyperparam_config["MODEL"].keys(), evolved_vals)}
+
+    def evaluate_with_params(param_array: np.ndarray):
+        assert len(param_array) == len(hyperparam_config["MODEL"])
+        hyperparam_dict = keys_with_evolved_vals(param_array)
+        model_res = model_func(hyperparam_dict)["result"]
+
+        return model_res if not mean_of_runs else np.mean(model_res)
+
+    es = cma.fmin(evaluate_with_params, initial_vals, sigma, **cma_kwargs)
+    x = keys_with_evolved_vals(es[0])
+
+    if return_only_best:
+        return {"result": x, "objective": es[1]}  # best evaluated solution, its objective function value
+    return {"result": x, "es_data": es[1:]}  # full cma.fmin output
+
+
 hyperparam_search_zoo = {
-    'GridSearch': perform_gridsearch
+    'GridSearch': perform_gridsearch,
+    'CMA-ES': cma_es
 }
