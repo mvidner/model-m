ad8e1e0f8e20dd888c19a96052654c37ffb67167
b3ck3tt <roman.neruda@gmail.com>
2020-05-10 23:44:45 +0200

    Update plot_test.py

--- tests/plot_test.py
+++ tests/plot_test.py
@@ -20,10 +20,10 @@ variant_dict = {}
 for variant in variants_list:
     variant_dict[variant] = [
         f"{BASEDIR}/history_{variant}_{i}.csv"
-        for i in range(0, 100)
+        for i in range(0, 30)
     ]
 
 
 plt.rcParams["figure.figsize"] = (20, 15)
 plot_mutliple_policies(variant_dict, group_days=2,
-                       group_func="max", save_path=filename, ylim=(0,500))
+                       group_func="max", save_path=filename)
