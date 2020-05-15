04f1906abd8961623b5897701cffff3e86fd7f4f
Gabriela Suchoparova <gabi.suchoparova@gmail.com>
2020-05-15 20:50:12 +0200

    Use mae in this branch:

--- hyperparam_search/eval_model.py
+++ hyperparam_search/eval_model.py
@@ -1,7 +1,7 @@
 import math
 
 from extended_network_model import STATES
-from sklearn.metrics import mean_squared_error
+from sklearn.metrics import mean_squared_error, mean_absolute_error
 
 
 def model_rmse(model, y_true):
@@ -9,6 +9,12 @@ def model_rmse(model, y_true):
     return math.sqrt(mean_squared_error(y_true, infected_count))
 
 
+def model_mae(model, y_true):
+    infected_count = model.model.get_state_count(STATES.I_d).values[1:]
+    return mean_absolute_error(y_true, infected_count)
+
+
 return_func_zoo = {
-    'rmse': model_rmse
+    'rmse': model_rmse,
+    'mae': model_mae
 }
