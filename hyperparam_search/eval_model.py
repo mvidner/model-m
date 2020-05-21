97c789e638fc481491ac7118cde4ac72c9e92691
Gabriela Suchoparova <gabi.suchoparova@gmail.com>
2020-05-21 16:06:05 +0200

    Added rsquared eval

--- hyperparam_search/eval_model.py
+++ hyperparam_search/eval_model.py
@@ -1,4 +1,5 @@
 import math
+import numpy as np
 
 from extended_network_model import STATES
 from sklearn.metrics import mean_squared_error, mean_absolute_error
@@ -14,7 +15,16 @@ def model_mae(model, y_true):
     return mean_absolute_error(y_true, infected_count)
 
 
+def model_r_squared(model, y_true):
+    infected_count = model.model.get_state_count(STATES.I_d).values[1:]
+    y_mean = np.mean(y_true)
+    tss = np.sum((y_true - y_mean) ** 2)
+    rss = np.sum((y_true - infected_count) ** 2)
+    return 1 - rss / tss
+
+
 return_func_zoo = {
     'rmse': model_rmse,
-    'mae': model_mae
+    'mae': model_mae,
+    'r2': model_r_squared
 }
