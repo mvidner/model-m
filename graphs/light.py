247de26bef554b71da492627d8681864ca2379a6
PetraVidnerova <petra.vidnerova@gmail.com>
2020-05-23 11:44:02 +0000

    confict fixed

--- graphs/light.py
+++ graphs/light.py
@@ -324,7 +324,6 @@ class LightGraph:
             print(f"Closing {name}", end="")
             i = self.layer_name.index(name)
             self.layer_weights[i] = 0 if not coefs else coefs[idx]
-<<<<<<< HEAD
 
     def copy(self):
         """
