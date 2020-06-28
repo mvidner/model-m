55ca6c4c26fc173d9957e4eab7e397de57735b56
Petra Vidnerová <petra.vidnerova@gmail.com>
2020-06-28 15:20:21 +0200

    fixed

--- tests/plot.sh
+++ tests/plot.sh
@@ -1,3 +1,3 @@
-#python plot_cmp.py smidovina_fitted smidovina_fitted
-python plot_t.py smidovina_fit10 smidovina_fit10
+python plot_cmp.py haklnv$1 haklnv$1
+#python plot_t.py smidovina_fit10 smidovina_fit10
 #python plot_test.py r1313 $NAMES
